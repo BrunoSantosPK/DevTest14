50e113890d71bfdaef206b71b0761b1a2f344ef7
BrunoSantosPK <bruno.ajsch@yahoo.com>
2020-06-13 14:54:22 -0300

    pequena correcao de layout

--- pages/renderTable.js
+++ pages/renderTable.js
@@ -1,13 +1,13 @@
 function renderTable(registros, idTabela) {
     let encontrados = "";
-    const w1 = "10%", w2 = "50%", w3 = "40%";
+    const w1 = "10%", w2 = "60%", w3 = "30%";
 
     for(let i = 0; i < registros.length; i++) {
         encontrados += `
             <tr>
                 <td style="width: ${w1}">${registros[i].id_cadastro}</td>
-                <td style="width: ${w2}">${registros[i].nome}</td>
-                <td style="width: ${w3}">${registros[i].nis}</td>
+                <td style="width: ${w2}; text-align: left;">${registros[i].nome}</td>
+                <td style="width: ${w3}; text-align: left;">${registros[i].nis}</td>
             </tr>`;
     }
 
@@ -15,8 +15,8 @@ function renderTable(registros, idTabela) {
     $(`#${idTabela}`).html(`
         <tr>
             <th style="width: ${w1}">ID</th>
-            <th style="width: ${w2}">Nome</th>
-            <th style="width: ${w3}">NIS</th>
+            <th style="width: ${w2}; text-align: left;">Nome</th>
+            <th style="width: ${w3}; text-align: left;">NIS</th>
         </tr>
         ${encontrados}
     `);
